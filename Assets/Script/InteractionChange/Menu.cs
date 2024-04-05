069fc0ae36418051a5928c4f45c0b7046d35bd78
Antoine Yon <antoine.yon01@gmail.com>
2024-04-05 17:17:08 +0200

    fix

--- Assets/Script/InteractionChange/Menu.cs
+++ Assets/Script/InteractionChange/Menu.cs
@@ -19,7 +19,7 @@ public class Menu : MonoBehaviour
     }
     public void Play()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void Quit() 
     {
