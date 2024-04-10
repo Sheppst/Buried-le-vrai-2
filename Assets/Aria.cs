6780a613c2e63f842e36369e3c4d6ca023945093
SkonkK <gasserelliot02@gmail.com>
2024-04-10 03:33:03 +0200

    Aria update (everything working good)

--- Assets/Aria.cs
+++ Assets/Aria.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Aria : MonoBehaviour
@@ -15,31 +16,42 @@ public class Aria : MonoBehaviour
     //For AttackPlayer Stage
     [Header("AttackPlayer")]
     [SerializeField] float AttackPlayerMoveSpeed;
+
+    // player check
     [SerializeField] Transform Player;
+    
     private Vector2 PlayerPosition;
+    private bool HasPlayerPosition;
+    
+
     //Other
     [SerializeField] Transform GroundCheck;
     [SerializeField] Transform RoofCheck;
     [SerializeField] Transform WallCheck;
-  
+    [SerializeField] Transform NotAllowedDistanceCheck;
+
+
     [SerializeField] float CheckRadius;
     [SerializeField] float CheckRadiusPlayer;
     [SerializeField] LayerMask GroundWallRoof_Layer;
     [SerializeField] LayerMask Player_Layer;
 
+    [SerializeField] private bool NotAllowedDistance;
     private bool IsTouchingRoof;
     private bool IsTouchingWall;
     private bool IsTouchingGround;
-   
+    
     private bool GoingUp = true;
     private bool FacingLeft = true;
     
     private Rigidbody2D enemyRB;
+    private Animator Enemyanimator;
     void Start()
     {
         IdelMoveDirection.Normalize();
         AttackMoveDirection.Normalize();
         enemyRB = GetComponent<Rigidbody2D>();
+        Enemyanimator = GetComponent<Animator>();   
     }
 
    
@@ -48,21 +60,52 @@ public class Aria : MonoBehaviour
         IsTouchingRoof = Physics2D.OverlapCircle(RoofCheck.position, CheckRadius, GroundWallRoof_Layer);
         IsTouchingWall = Physics2D.OverlapCircle(WallCheck.position, CheckRadius, GroundWallRoof_Layer);
         IsTouchingGround = Physics2D.OverlapCircle(GroundCheck.position, CheckRadius, GroundWallRoof_Layer);
-        
-       
-            IdelState();
-        
-        
-       //AttackUpAndDownState();
-      
-            
-        //AttackPlayer();
-        
-        //FlipTowardsPlayer();
-    }
+        NotAllowedDistance = Physics2D.OverlapCircle(NotAllowedDistanceCheck.position, CheckRadiusPlayer, Player_Layer);
 
+        AllowedDistance();
+
+
+        //IdelState();
+
+
+        //AttackUpAndDownState();
+
+
+        //AttackPlayer();
+
+
+    }
+    private void RandomStatePicker()
+    {
+        int randomState = Random.Range(0, 2);
+        if (randomState == 0)
+        {
+            Enemyanimator.Play("AttackUpAndDown");
+        }
+        else if(randomState == 1 && NotAllowedDistance ==false)
+        {
+            Enemyanimator.Play("AttackPlayer");
+        }
+        
+    }
+    private void AllowedDistance()
+    {
+        if (NotAllowedDistance == false)
+        {
+            Enemyanimator.SetTrigger("AllowedDistance");
+
+        }
+        else if (NotAllowedDistance == true)
+        {
+            Enemyanimator.ResetTrigger("AllowedDistance");
+        }
+
+
+    }
     public void IdelState()
     {
+        Enemyanimator.ResetTrigger("Slam");
+        
         if (IsTouchingRoof && GoingUp) 
         {
             ChangeDirection();
@@ -82,7 +125,7 @@ public class Aria : MonoBehaviour
                 Flip();
             }
         }
-          enemyRB.velocity = IdelMoveSpeed * IdelMoveDirection;
+          enemyRB.velocity = IdelMoveSpeed * IdelMoveDirection * Time.deltaTime;
     }
 
    public void AttackUpAndDownState()
@@ -106,17 +149,32 @@ public class Aria : MonoBehaviour
                 Flip();
             }
         }
-        enemyRB.velocity = AttackMoveSpeed * AttackMoveDirection;
+        enemyRB.velocity = AttackMoveSpeed * AttackMoveDirection * Time.deltaTime;
     }
 
     public void AttackPlayer()
     {
-        // take player position 
-        PlayerPosition = Player.position - transform.position;
-        // normalize player position
-        PlayerPosition.Normalize();
-        // Attack On that position
-        enemyRB.velocity = PlayerPosition * AttackPlayerMoveSpeed;
+        if (!HasPlayerPosition)
+        {
+            PlayerPosition = Player.position - transform.position;
+            PlayerPosition.Normalize();
+            HasPlayerPosition = true;
+        }
+        if (HasPlayerPosition)
+        
+        {
+            FlipTowardsPlayer();
+            enemyRB.velocity = PlayerPosition * AttackPlayerMoveSpeed * Time.deltaTime;
+        }
+       if (IsTouchingGround || IsTouchingWall)
+        {
+            enemyRB.velocity = Vector2.zero;
+            HasPlayerPosition= false;
+            Enemyanimator.SetTrigger("Slam");
+            Enemyanimator.ResetTrigger("AttackPlayer");
+        }
+        
+        
     }
 
     void FlipTowardsPlayer ()
@@ -156,6 +214,7 @@ public class Aria : MonoBehaviour
         Gizmos.DrawWireSphere(WallCheck.position, CheckRadius);
         
         Gizmos.DrawWireSphere(RoofCheck.position, CheckRadius);
-        
+        Gizmos.DrawWireSphere(NotAllowedDistanceCheck.position, CheckRadiusPlayer);
+
     }
 }
---
+++ Assets/AttakcPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttakcPlayer : StateMachineBehaviour
+{
+
+    [SerializeField] Aria Aria;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Aria = GameObject.FindGameObjectWithTag("Aria").GetComponent<Aria>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Aria.AttackPlayer();
+    }
+
+
+}
