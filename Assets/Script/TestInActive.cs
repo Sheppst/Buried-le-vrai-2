56d00f12ba2b463e9c834af8da933e2bf1549042
Antoine Yon <antoine.yon01@gmail.com>
2024-04-12 22:19:56 +0200

    .

--- Assets/Script/TestInActive.cs
+++ Assets/Script/TestInActive.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TestInActive : MonoBehaviour
 {
+    private bool OnLerp;
+    public float speed = 3;
+    private float Zslasher = 1;
     private void Awake()
     {
         
@@ -11,6 +14,7 @@ public class TestInActive : MonoBehaviour
     private void OnEnable()
     {
         transform.position = new Vector3(-16, 1, 0);
+        OnLerp = false;
     }
     // Start is called before the first frame update
     void Start()
@@ -21,6 +25,16 @@ public class TestInActive : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            OnLerp = true;
+        }
+        if (OnLerp)
+        {
+            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector3.Lerp(transform.position, target - Vector3.forward * Zslasher, speed * Time.deltaTime);
+            OnLerp = false;
+            Zslasher++;
+        }
     }
 }
