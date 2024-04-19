c94c7c066b2dd87751d0315fcd542627725adce6
Antoine Yon <antoine.yon01@gmail.com>
2024-04-19 11:21:35 +0200

    chgt cam

---
+++ Assets/Script/FollowCam/CamPos.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPos : MonoBehaviour
+{
+    [SerializeField] float EcartementCam;
+    [SerializeField] float CamSpeed;
+    private bool turn;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Target = new Vector3();
+        if (transform.parent.localScale.x > 0)
+        {
+            Target = transform.parent.position + Vector3.right * EcartementCam;
+        }
+        else if (transform.parent.localScale.x < 0)
+        {
+            Target = transform.parent.position + Vector3.left * EcartementCam;
+        }
+    }
+}
