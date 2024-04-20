a8612b80f9331959dd7fdc97ce0d8f031f76b5e0
SkonkK <gasserelliot02@gmail.com>
2024-04-21 00:23:46 +0200

    ajout de plateforme qui bouge + plateforme qui tombe

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,56 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Elliot Script
+
+    public Transform StartPoint;
+    public Transform EndPoint;
+    public Transform Platform;
+    private int direction = 1;
+    public float speed = 1f;
+
+
+    void Update()
+    {
+        Vector2 target = currentMovementTarget();
+
+        Platform.position = Vector2.Lerp(Platform.position, target, speed * Time.deltaTime);
+
+        float distance = (target - (Vector2)Platform.position).magnitude;
+
+        if (distance <= 0.1f)
+        {
+            direction *= -1;
+        }
+    }
+
+
+    Vector2 currentMovementTarget()
+    {
+        if (direction == 1)
+        {
+            return StartPoint.position;
+        }
+        else 
+        {
+            return EndPoint.position;
+        }
+    }
+
+   
+    private void OnDrawGizmos()
+    {
+        if (Platform != null && StartPoint != null && EndPoint != null)
+        {
+            Gizmos.DrawLine(Platform.transform.position, StartPoint.position);
+            Gizmos.DrawLine(Platform.transform.position, EndPoint.position);
+
+        }
+    }
+}
---
+++ Assets/fallingPlatform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatform : MonoBehaviour
+{
+    // Elliot Script
+    [SerializeField] private float FallDelay;
+    [SerializeField] float DestroyDelay;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(FallDelay);
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, DestroyDelay);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+           StartCoroutine(Fall());
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
