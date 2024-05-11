91f63a8796317db4f64ac322da54990a8a7d6c85
Antoine Yon <antoine.yon01@gmail.com>
2024-05-11 17:15:20 +0200

    Modif menu

--- Assets/Script/Button/OptionPannel.cs
+++ Assets/Script/Button/OptionPannel.cs
@@ -8,7 +8,7 @@ public class OptionPannel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        InfoBulleGM.SetActive(false);
     }
 
     // Update is called once per frame
