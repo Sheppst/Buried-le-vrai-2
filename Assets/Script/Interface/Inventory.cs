73556b26aaf8e1f9a3fc649278aac419c31bc13d
SkonkK <gasserelliot02@gmail.com>
2024-05-13 00:37:52 +0200

    Systeme d'ajout de potion et de cap de nombre de potions

--- Assets/Script/Interface/Inventory.cs
+++ Assets/Script/Interface/Inventory.cs
@@ -8,14 +8,18 @@ public class Inventory : MonoBehaviour
 {
     [Header("Mana")]
     public int manaPotAmount;
-    [HideInInspector] public int maxManaPot =5;
+    [HideInInspector] public int maxManaPot = 5;
     private bool drinkMana;
+    public bool addManaPot;
     private Mana mana;
     public TextMeshProUGUI manaTextMeshPro;
+
+
     [Header("Life")]
     public int lifePotAmount;
     [HideInInspector] public int maxLifePot = 5;
     private bool drinkLife;
+    public bool addLifePot;
     private PlayerLifeSystem playerLifeSystem;
     public TextMeshProUGUI lifeTextMeshPro;
     
@@ -33,24 +37,74 @@ public class Inventory : MonoBehaviour
     }	
     void Update()
     {
+        //Input
         if (Input.GetKeyDown(KeyCode.E) && manaPotAmount > 0)
         {
             drinkMana = true;
-        }
-        if (drinkMana) 
-        {
-            DrinkManaPotion();
         }
         if (Input.GetKeyDown(KeyCode.F) && lifePotAmount > 0)
         {
             drinkLife = true;
         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            addManaPot = true;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            addLifePot = true;
+        }
+        //Drink
+        if (drinkMana)
+        {
+            DrinkManaPotion();
+        }
         if (drinkLife)
         {
             DrinkLifePotion();
         }
+        //Text
         lifeTextMeshPro.text =  lifePotAmount.ToString();
         manaTextMeshPro.text = manaPotAmount.ToString();
+
+        //AddPotion
+        if (addManaPot)
+        {
+            AddManaPotion();
+        }
+
+        if (addLifePot)
+        {
+            AddLifePotion();
+        }
+       
+    }
+    
+    public void AddManaPotion()
+    {
+        if ( manaPotAmount < 5)
+        {
+            manaPotAmount = manaPotAmount + 1;
+            addManaPot = false;
+        }
+        else if (manaPotAmount == 5)
+        {
+            addManaPot = false;
+        }
+        
+    }
+    public void AddLifePotion()
+    {
+        if (lifePotAmount < 5)
+        {
+            lifePotAmount = lifePotAmount + 1;
+            addLifePot = false;
+        }
+        else if (lifePotAmount == 5)
+        {
+            addLifePot = false;
+        }
+
     }
 
     void DrinkManaPotion()
