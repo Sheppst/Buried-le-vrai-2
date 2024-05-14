ae885a0e1ea7c6d50bcfd53c3922839d56d7a132
SkonkK <gasserelliot02@gmail.com>
2024-05-14 17:59:27 +0200

    changes

--- Assets/Script/Projectile/Gun.cs
+++ Assets/Script/Projectile/Gun.cs
@@ -11,45 +11,52 @@ public class Gun : MonoBehaviour
     [SerializeField] private Sprite Balle;
     [SerializeField] private Animator anim;
     [SerializeField] private float SpeedBullet = 20;
-    // Start is called before the first frame update
+    [SerializeField] private float fireRate = 0.5f; // Délai entre chaque tir en secondes
+    private float nextFireTime = 0f; // Temps avant le prochain tir
+
     void Awake()
     {
         mana = GetComponent<Mana>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        if (Input.GetMouseButton(0)) 
+
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
-            if (transform.position.x - target.x < 0)
+            if (mana.manaPool >= bulletCost)
             {
-                Vector3 NewSight = transform.localScale;
-                NewSight.x = Mathf.Abs(NewSight.x);
-                transform.localScale = NewSight;
-            }
-            else if (transform.position.x - target.x > 0)
-            {
-                Vector3 NewSight = transform.localScale;
-                NewSight.x = Mathf.Abs(NewSight.x) * -1;
-                transform.localScale = NewSight;
+                mana.SpendMana(bulletCost);
+                anim.SetTrigger("IsShootting");
+                Shoot(target);
+                nextFireTime = Time.time + fireRate; // Met à jour le temps avant le prochain tir
             }
         }
-        if (Input.GetMouseButtonDown(0) && mana.manaPool >= bulletCost) 
+    }
+
+    void Shoot(Vector3 target)
+    {
+        if (transform.position.x - target.x < 0)
         {
-            mana.SpendMana(bulletCost);
-            anim.SetTrigger("IsShootting");
-            GameObject projectile = Proj;
-            projectile.transform.localScale = Vector3.one * 0.042198f;
-            projectile.GetComponent<AttackObject>().Target = target;
-            projectile.GetComponent<AttackObject>().Speed = SpeedBullet;
-            projectile.GetComponent<AttackObject>().Thrower = tag;
-            projectile.GetComponent<AttackObject>().tag = "ProjPlayer";
-            projectile.GetComponent<AttackObject>().TimeBeforeDestroy = 5;
-            projectile.GetComponent<AttackObject>().ObjSprite = Balle;
-            Instantiate(projectile,FirePos.position,Quaternion.identity);
+            Vector3 NewSight = transform.localScale;
+            NewSight.x = Mathf.Abs(NewSight.x);
+            transform.localScale = NewSight;
         }
+        else if (transform.position.x - target.x > 0)
+        {
+            Vector3 NewSight = transform.localScale;
+            NewSight.x = Mathf.Abs(NewSight.x) * -1;
+            transform.localScale = NewSight;
+        }
+
+        GameObject projectile = Instantiate(Proj, FirePos.position, Quaternion.identity);
+        projectile.transform.localScale = Vector3.one * 0.042198f;
+        projectile.GetComponent<AttackObject>().Target = target;
+        projectile.GetComponent<AttackObject>().Speed = SpeedBullet;
+        projectile.GetComponent<AttackObject>().Thrower = tag;
+        projectile.GetComponent<AttackObject>().tag = "ProjPlayer";
+        projectile.GetComponent<AttackObject>().TimeBeforeDestroy = 5;
+        projectile.GetComponent<AttackObject>().ObjSprite = Balle;
     }
 }
